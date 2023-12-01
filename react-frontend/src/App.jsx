2b2f3244851461f711754c3d91e9f01eca7037d3
jake <jake.imanaka@gmail.com>
2023-12-01 04:42:32 -1000

    add simple 404 page

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -6,12 +6,14 @@ import Landing from "./containers/Landing";
 import Home from "./containers/Home";
 import LabsHome from "./containers/LabsHome";
 import Sandbox from "./containers/Sandbox";
+import NotFound from "./containers/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/labs" element={<LabsHome />} />
