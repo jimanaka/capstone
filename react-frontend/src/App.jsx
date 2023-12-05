852ba740db94affb2367a09c84c987a3195242c4
jake <jake.imanaka@gmail.com>
2023-12-05 01:28:59 -1000

    transition to tailwind

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -3,21 +3,15 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Landing from "./containers/Landing";
-import Home from "./containers/Home";
-import LabsHome from "./containers/LabsHome";
-import Sandbox from "./containers/Sandbox";
 import NotFound from "./containers/NotFound";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-ctp-base">
       <Navbar />
       <Routes>
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/labs" element={<LabsHome />} />
-        <Route path="/sandbox" element={<Sandbox />} />
       </Routes>
     </div>
   );
--- react-frontend/src/containers/Landing.jsx
+++ react-frontend/src/containers/Landing.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
 
 const Landing = () => {
   return (
     <div>
-      <Button component={Link} to="/home">
         Go here
-      </Button>
     </div>
   );
 };
