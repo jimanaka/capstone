368535d25a23519c684036cf27cc51657e0bbaaa
jake <jake.imanaka@gmail.com>
2023-12-28 09:59:36 -1000

    skeleton payload generator

---
+++ react-frontend/src/components/PayloadGenerator.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CodeView from "./CodeView";
+
+const PayloadGenerator = () => {
+  return (
+    <div className="m-5 flex h-[35rem] justify-center space-x-4">
+      <CodeView>stuff goes here</CodeView>
+    </div>
+  )
+};
+
+export default PayloadGenerator;
