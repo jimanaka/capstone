d69469484f3e7f7e7a34e7497bd793de411df000
jake <jake.imanaka@gmail.com>
2023-12-29 11:19:40 -1000

    initial course page

--- react-frontend/src/containers/Courses.jsx
+++ react-frontend/src/containers/Courses.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+
+import CourseCard from "../components/CourseCard";
+
 const Courses = () => {
-  return <div>courses page</div>;
+  return (
+    <div className="container">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-6xl font-extrabold">My Courses</h1>
+        <button className="btn-primary mt-4 inline-flex items-center rounded-full">
+          <PlusCircleIcon className="mr-2 h-8 w-8" />
+          <span>Add Course</span>
+        </button>
+      </div>
+      <div className="grid grid-flow-row grid-cols-3 gap-4 py-8">
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
