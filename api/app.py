5247fae910b2479e6b53f98b714f07f1377eb636
jake <jake.imanaka@gmail.com>
2023-12-30 09:04:58 -1000

    swap to mongo from sql

--- api/app.py
+++ api/app.py
@@ -1,8 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config.from_object("src.config.Config")
 
+mongo = PyMongo(app)
+db = mongo.db
+
 @app.route("/")
 def hello_world():
     return jsonify(hello="world");
+
--- api/src/config.py
+++ api/src/config.py
@@ -3,5 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MONGO_URI = "mongodb://" + os.getenv("MONGO_INITDB_ROOT_USERNAME") + ":" + os.getenv("MONGO_INITDB_ROOT_PASSWORD") + "@" + os.getenv("MONGO_HOSTNAME") + ':27017/' + os.getenv("MONGO_DATABASE") # type: ignore
