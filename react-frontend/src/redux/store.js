191093b6e8fcd0915d646b22054f19938974c3fc
Jake Imanaka <jake.imanaka@gmail.com>
2024-03-15 23:45:15 -1000

    initial payload generator ui and redux store

--- react-frontend/src/redux/store.js
+++ react-frontend/src/redux/store.js
@@ -6,6 +6,7 @@ import sessionReducer from "./slice/sessionSlice";
 import courseReducer from "./slice/courseSlice";
 import createCourseReducer from "./slice/createCourseSlice"
 import sessionMiddleware from "./middleware/socketMiddleware";
+import payloadGeneratorReducer from "./slice/payloadGenerator";
 
 export default configureStore({
   reducer: {
@@ -15,6 +16,7 @@ export default configureStore({
     session: sessionReducer,
     course: courseReducer,
     createCourse: createCourseReducer,
+    payloadGenerator: payloadGeneratorReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
